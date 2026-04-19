f9e7619b8d54f99d570eddd89d168cfdc0cd5003
Sharafath <Sharafath@SHARAFATH-PC>
2026-04-19 17:25:06 -0400

    Change video source to video.mp4 per user request

--- src/components/AboutSnippet.tsx
+++ src/components/AboutSnippet.tsx
@@ -51,7 +51,7 @@ export function AboutSnippet() {
        >
          <video
            ref={videoRef}
-           src="/ig.mp4"
+           src="/video.mp4"
            poster="/thumbnail.jpg"
            preload="metadata"
            playsInline
